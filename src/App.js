8ee97868304d680960792e91a9004a6b48e10ddf
Manmeet Singh <gdm.manmeet@gmail.com>
2023-06-14 16:48:23 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/navbar';
+import Counters from './components/counters';
+
+
+class App extends Component {
+//kirtan name, artist, duration, date, album, url
+      state = { 
+        counters: [
+            { id: 1, value: 4},
+            { id: 2, value: 0},
+            { id: 3, value: 0},
+            { id: 4, value: 0},
+            { id: 5, value: 0}
+        ]
+    };
+
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({ counters });
+    }
+
+    handleDelete = (counterId) =>
+    {
+        //this.setState({ value : this.state.value - 1 });
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        //this.setState({ counters : counters}); 
+        this.setState({counters}); 
+    }
+
+    handleReset = () =>
+    {
+        /* this.state.counters.forEach(function (value) 
+          {if (value.value>2)
+            {console.log(value.value)} 
+          else {console.log("hello")}
+        }
+        ); */
+        const counters = this.state.counters.map (c => { 
+            c.value = 0; 
+            return c;
+        });
+        
+
+
+        this.setState({ counters });
+    }
+
+render() {
+  return (
+    <React.Fragment>
+      <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+      <main className='container'>
+          <Counters 
+          counters = {this.state.counters}
+          onReset = {this.handleReset} 
+          onIncrement = {this.handleIncrement} 
+          onDelete = {this.handleDelete} />
+      </main>
+    </React.Fragment>
+  );
+}
+}
+
+export default App;
---
+++ src/components/counters.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+
+
+    render() { 
+        return ( 
+            <div>
+                <button onClick = {this.props.onReset} className="btn btn-primary btm-sm m-2">Reset</button>   
+                {this.props.counters.map( counter => (
+                /* <Counter 
+                    key={counter.id} 
+                    onDelete={this.handleDelete} 
+                    value={counter.value} 
+                    id={counter.id}
+                />
+                ) ) 
+                */
+                 // when the object size increases, the variables to bemapped also increase, 
+                 // so we can directly write counter = {counter} as shown below
+                 <Counter key={counter.id} onIncrement = {this.props.onIncrement} onDelete={this.props.onDelete} counter={counter} />) )
+                }
+            </div>
+        );
+    }
+}
+ 
+export default Counters;
